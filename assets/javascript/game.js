b575801fd10ed279d1ce7bd1dbf20c04a6cc05c6
Michael Garland <mgarland@pmgnet.local>
2018-02-27 16:39:43 -0500

    rough draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,39 @@
+//Determine Computer letter
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+//Creating variables to hold the number of wins, losses and guesses
 var wins = 0;
 var lossesTot = 0;
 var guessesLeft = 9;
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = lossesTot;
-document.getElementById("guesses").innerHTML = guessesLeft;
 
-
-//Determine Computer letter
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(getRandomLetter);
-
-
-//log user guess
+//This function is run whenver the user presses a key.
 document.onkeyup = function (event) {
-  var userGuess = event.key.toLowerCase();
-  console.log(userGuess);
-  if (userGuess === getRandomLetter) {
-    wins++;
-    guessesLeft = 9;
-    getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-  } else {
-    guessesLeft--;
-  };
-  document.getElementById("wins").innerHTML = wins;
-  document.getElementById("losses").innerHTML = lossesTot;
-  document.getElementById("guesses").innerHTML = guessesLeft;
+
+    //Determines the key and puts it in lowercase
+    var userGuess = event.key.toLowerCase();
+
+    //Randomly shooses a choice from the option alphabet string
+    var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    //This logic determines the out come of the round (win/loss/tie), and increments the approptiate number
+    if (userGuess === getRandomLetter) {
+        wins++;
+        guessesLeft = 9;
+        getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    } else {
+        guessesLeft--;
+    };
+
+    //This logic determines what to do if the person runs out of turns
+    if (guessesLeft < 1) {
+        lossesTot++;
+        var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        guessesLeft = 9;
+    }
+
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("lossesTot").innerHTML = lossesTot;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
 }
 
 
